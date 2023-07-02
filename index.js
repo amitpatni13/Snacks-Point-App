91362612fb59dbeca1fae47770d52d709f5f3b65
amitpatni13 <amitpatni13@gmail.com>
2023-07-02 20:53:52 +0530

    app complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const foodMenu = [
     //Pizza Menu
     {
-        logo: "assets/images/bacon-pizza.png",
+        logo: "assets/images/pizza.png",
         name: "Bacon Pizza",
         price: "$44",
         rating: "4.5",
@@ -9,7 +9,7 @@ const foodMenu = [
         category: "Pizza",
     },
     {
-        logo: "assets/images/chillipaper-pizza.png",
+        logo: "assets/images/pizza.png",
         name: "Chillipaper Pizza",
         price: "$39",
         rating: "4.2",
@@ -17,7 +17,7 @@ const foodMenu = [
         category: "Pizza",
     },
     {
-        logo: "assets/images/mariana-pizza.png",
+        logo: "assets/images/pizza.png",
         name: "Mariana Pizza",
         price: "$38",
         rating: "4.7",
@@ -25,7 +25,7 @@ const foodMenu = [
         category: "Pizza",
     },
     {
-        logo: "assets/images/mozerella-pizza.png",
+        logo: "assets/images/pizza.png",
         name: "Mozerella Pizza",
         price: "$42",
         rating: "4.3",
@@ -33,7 +33,7 @@ const foodMenu = [
         category: "Pizza",
     },
     {
-        logo: "assets/images/mexican-pizza.png",
+        logo: "assets/images/pizza.png",
         name: "Mexican Pizza",
         price: "$36",
         rating: "4.8",
@@ -41,7 +41,7 @@ const foodMenu = [
         category: "Pizza",
     },
     {
-        logo: "assets/images/mushroom-pizza.png",
+        logo: "assets/images/pizza.png",
         name: "Mushroom Pizza",
         price: "$40",
         rating: "4.4",
@@ -50,7 +50,7 @@ const foodMenu = [
     },
     // Biryani Menu
     {
-        logo: "assets/images/biryani1.jpg",
+        logo: "assets/images/biryani.png",
         name: "Hyderabadi Biryani",
         price: "$12",
         rating: "4.5",
@@ -58,7 +58,7 @@ const foodMenu = [
         category: "Biryani",
     },
     {
-        logo: "assets/images/biryani2.jpg",
+        logo: "assets/images/biryani.png",
         name: "Lucknowi Biryani",
         price: "$10",
         rating: "4.2",
@@ -66,7 +66,7 @@ const foodMenu = [
         category: "Biryani",
     },
     {
-        logo: "assets/images/biryani3.jpg",
+        logo: "assets/images/biryani.png",
         name: "Kolkata Biryani",
         price: "$11",
         rating: "4.7",
@@ -74,7 +74,7 @@ const foodMenu = [
         category: "Biryani",
     },
     {
-        logo: "assets/images/biryani4.jpg",
+        logo: "assets/images/biryani.png",
         name: "Bombay Biryani",
         price: "$13",
         rating: "4.3",
@@ -82,7 +82,7 @@ const foodMenu = [
         category: "Biryani",
     },
     {
-        logo: "assets/images/biryani5.jpg",
+        logo: "assets/images/biryani.png",
         name: "Vegetable Biryani",
         price: "$9",
         rating: "4.1",
@@ -90,7 +90,7 @@ const foodMenu = [
         category: "Biryani",
     },
     {
-        logo: "assets/images/biryani6.jpg",
+        logo: "assets/images/biryani.png",
         name: "Egg Biryani",
         price: "$10",
         rating: "4.6",
@@ -99,54 +99,249 @@ const foodMenu = [
     },
     // Chicken Category
     {
-        logo: 'assets/images/chicken1.png',
-        name: 'Grilled Chicken',
-        price: '$12',
-        rating: '4.5',
+        logo: "assets/images/chicken.png",
+        name: "Grilled Chicken",
+        price: "$12",
+        rating: "4.5",
         isItemInCart: false,
-        category: 'Chicken'
-      },
-      {
-        logo: 'assets/images/chicken2.png',
-        name: 'Chicken Curry',
-        price: '$10',
-        rating: '4.2',
+        category: "Chicken",
+    },
+    {
+        logo: "assets/images/chicken.png",
+        name: "Chicken Curry",
+        price: "$10",
+        rating: "4.2",
         isItemInCart: false,
-        category: 'Chicken'
-      },
-      {
-        logo: 'assets/images/chicken3.png',
-        name: 'Roasted Chicken',
-        price: '$14',
-        rating: '4.8',
+        category: "Chicken",
+    },
+    {
+        logo: "assets/images/chicken.png",
+        name: "Roasted Chicken",
+        price: "$14",
+        rating: "4.8",
         isItemInCart: false,
-        category: 'Chicken'
-      },
-      {
-        logo: 'assets/images/chicken4.png',
-        name: 'Chicken Shawarma',
-        price: '$8',
-        rating: '4.3',
+        category: "Chicken",
+    },
+    {
+        logo: "assets/images/chicken.png",
+        name: "Chicken Shawarma",
+        price: "$8",
+        rating: "4.3",
         isItemInCart: false,
-        category: 'Chicken'
-      },
-      {
-        logo: 'assets/images/chicken5.png',
-        name: 'Chicken Biryani',
-        price: '$15',
-        rating: '4.6',
+        category: "Chicken",
+    },
+    {
+        logo: "assets/images/chicken.png",
+        name: "Chicken Biryani",
+        price: "$15",
+        rating: "4.6",
         isItemInCart: false,
-        category: 'Chicken'
-      },
-      {
-        logo: 'assets/images/chicken6.png',
-        name: 'Fried Chicken',
-        price: '$9',
-        rating: '4.4',
+        category: "Chicken",
+    },
+    {
+        logo: "assets/images/chicken.png",
+        name: "Fried Chicken",
+        price: "$9",
+        rating: "4.4",
         isItemInCart: false,
-        category: 'Chicken'
-      }
-    // Similarly give me card data with 6 items each for Paneer Category, Vegitable Category, South Indian Category and Chinese Category via Chatgpt
+        category: "Chicken",
+    },
+    // Paneer Category
+    {
+        logo: "assets/images/paneer.png",
+        name: "Paneer Tikka",
+        price: "$8",
+        rating: "4.5",
+        isItemInCart: false,
+        category: "Paneer",
+    },
+    {
+        logo: "assets/images/paneer.png",
+        name: "Palak Paneer",
+        price: "$10",
+        rating: "4.2",
+        isItemInCart: false,
+        category: "Paneer",
+    },
+    {
+        logo: "assets/images/paneer.png",
+        name: "Paneer Butter Masala",
+        price: "$12",
+        rating: "4.8",
+        isItemInCart: false,
+        category: "Paneer",
+    },
+    {
+        logo: "assets/images/paneer.png",
+        name: "Kadai Paneer",
+        price: "$9",
+        rating: "4.3",
+        isItemInCart: false,
+        category: "Paneer",
+    },
+    {
+        logo: "assets/images/paneer.png",
+        name: "Paneer Biryani",
+        price: "$15",
+        rating: "4.6",
+        isItemInCart: false,
+        category: "Paneer",
+    },
+    {
+        logo: "assets/images/paneer.png",
+        name: "Paneer Paratha",
+        price: "$6",
+        rating: "4.4",
+        isItemInCart: false,
+        category: "Paneer",
+    },
+    // Vegitable Category
+    {
+        logo: "assets/images/vegetable.png",
+        name: "Mixed Vegetable Curry",
+        price: "$10",
+        rating: "4.5",
+        isItemInCart: false,
+        category: "Vegetable",
+    },
+    {
+        logo: "assets/images/vegetable.png",
+        name: "Stir-Fried Vegetables",
+        price: "$12",
+        rating: "4.2",
+        isItemInCart: false,
+        category: "Vegetable",
+    },
+    {
+        logo: "assets/images/vegetable.png",
+        name: "Grilled Vegetables",
+        price: "$9",
+        rating: "4.8",
+        isItemInCart: false,
+        category: "Vegetable",
+    },
+    {
+        logo: "assets/images/vegetable.png",
+        name: "Vegetable Biryani",
+        price: "$15",
+        rating: "4.3",
+        isItemInCart: false,
+        category: "Vegetable",
+    },
+    {
+        logo: "assets/images/vegetable.png",
+        name: "Vegetable Salad",
+        price: "$8",
+        rating: "4.6",
+        isItemInCart: false,
+        category: "Vegetable",
+    },
+    {
+        logo: "assets/images/vegetable.png",
+        name: "Vegetable Pizza",
+        price: "$14",
+        rating: "4.4",
+        isItemInCart: false,
+        category: "Vegetable",
+    },
+    // South Indian Category
+    {
+        logo: "assets/images/south-indian.png",
+        name: "Dosa",
+        price: "$6",
+        rating: "4.5",
+        isItemInCart: false,
+        category: "South Indian",
+    },
+    {
+        logo: "assets/images/south-indian.png",
+        name: "Idli",
+        price: "$5",
+        rating: "4.2",
+        isItemInCart: false,
+        category: "South Indian",
+    },
+    {
+        logo: "assets/images/south-indian.png",
+        name: "Vada",
+        price: "$4",
+        rating: "4.8",
+        isItemInCart: false,
+        category: "South Indian",
+    },
+    {
+        logo: "assets/images/south-indian.png",
+        name: "Sambar",
+        price: "$3",
+        rating: "4.3",
+        isItemInCart: false,
+        category: "South Indian",
+    },
+    {
+        logo: "assets/images/south-indian.png",
+        name: "Rasam",
+        price: "$4",
+        rating: "4.6",
+        isItemInCart: false,
+        category: "South Indian",
+    },
+    {
+        logo: "assets/images/south-indian.png",
+        name: "Pongal",
+        price: "$7",
+        rating: "4.4",
+        isItemInCart: false,
+        category: "South Indian",
+    },
+    // Chinese Category
+    {
+        logo: "assets/images/chinese.png",
+        name: "Fried Rice",
+        price: "$10",
+        rating: "4.5",
+        isItemInCart: false,
+        category: "Chinese",
+    },
+    {
+        logo: "assets/images/chinese.png",
+        name: "Sweet and Sour Chicken",
+        price: "$12",
+        rating: "4.2",
+        isItemInCart: false,
+        category: "Chinese",
+    },
+    {
+        logo: "assets/images/chinese.png",
+        name: "Kung Pao Shrimp",
+        price: "$14",
+        rating: "4.8",
+        isItemInCart: false,
+        category: "Chinese",
+    },
+    {
+        logo: "assets/images/chinese.png",
+        name: "Egg Drop Soup",
+        price: "$8",
+        rating: "4.3",
+        isItemInCart: false,
+        category: "Chinese",
+    },
+    {
+        logo: "assets/images/chinese.png",
+        name: "Spring Rolls",
+        price: "$6",
+        rating: "4.6",
+        isItemInCart: false,
+        category: "Chinese",
+    },
+    {
+        logo: "assets/images/chinese.png",
+        name: "General Tso's Chicken",
+        price: "$11",
+        rating: "4.4",
+        isItemInCart: false,
+        category: "Chinese",
+    },
 ];
 
 let cart = [];
@@ -287,84 +482,88 @@ function displayCart() {
     const menuCard = document.getElementById("menu-card");
     menuCard.innerHTML = ""; // Resetting the inner html if already items present
 
+    document.getElementById("menu-category").innerText = '';
 
-    const cartBody = document.createElement('div');
-    cartBody.className = 'cart-body'
+    const cartBody = document.createElement("div");
+    cartBody.className = "cart-body";
 
     // Create the heading
-    const heading = document.createElement('h2');
-    heading.textContent = 'Items Added in Wishlist';
+    const heading = document.createElement("h2");
+    heading.textContent = "Items Added in Wishlist";
     cartBody.appendChild(heading);
-    
+
     // Create the table
-    const table = document.createElement('table');
-    table.className = 'table table-borderless';
-    
+    const table = document.createElement("table");
+    table.className = "table table-borderless";
+
     // Create the table head
-    const thead = document.createElement('thead');
-    const headRow = document.createElement('tr');
-    
-    const headItem1 = document.createElement('th');
-    headItem1.scope = 'col';
-    headItem1.textContent = 'Item';
-    
-    const headItem2 = document.createElement('th');
-    headItem2.scope = 'col';
-    headItem2.textContent = 'Name';
-    
-    const headItem3 = document.createElement('th');
-    headItem3.scope = 'col';
-    headItem3.textContent = 'Price ($)';
-    
+    const thead = document.createElement("thead");
+    const headRow = document.createElement("tr");
+
+    const headItem1 = document.createElement("th");
+    headItem1.scope = "col";
+    headItem1.textContent = "Item";
+
+    const headItem2 = document.createElement("th");
+    headItem2.scope = "col";
+    headItem2.textContent = "Name";
+
+    const headItem3 = document.createElement("th");
+    headItem3.scope = "col";
+    headItem3.textContent = "Price ($)";
+
     headRow.appendChild(headItem1);
     headRow.appendChild(headItem2);
     headRow.appendChild(headItem3);
     thead.appendChild(headRow);
     table.appendChild(thead);
-    
-    // Create the table body
-    const tbody = document.createElement('tbody');
-    
-    // Adding items in cart dynamically
-    cart.forEach(item => {
-      const row = document.createElement('tr');
-    
-      const logoCell = document.createElement('td');
-      const logoImg = document.createElement('img');
-      logoImg.src = item.logo;
-      logoImg.className = 'cart-img-logo';
-      logoImg.alt = 'Food Logo';
-      logoCell.appendChild(logoImg);
-    
-      const nameCell = document.createElement('td');
-      nameCell.textContent = item.name;
-    
-      const priceCell = document.createElement('td');
-      priceCell.textContent = item.price;
-    
-      row.appendChild(logoCell);
-      row.appendChild(nameCell);
-      row.appendChild(priceCell);
-    
-      tbody.appendChild(row);
-    });
-    
-    // Create the total row
-    const totalRow = document.createElement('tr');
-    
-    const emptyCell = document.createElement('td');
-    const emptyCell2 = document.createElement('td');
-    const totalCell = document.createElement('td');
 
-    const totalAmt = cart.reduce((total, item) => total + Number(item.price.substring(1)), 0);
-    totalCell.textContent = '$' + totalAmt;
-    
+    // Create the table body
+    const tbody = document.createElement("tbody");
+
+    // Adding items in cart dynamically
+    cart.forEach((item) => {
+        const row = document.createElement("tr");
+
+        const logoCell = document.createElement("td");
+        const logoImg = document.createElement("img");
+        logoImg.src = item.logo;
+        logoImg.className = "cart-img-logo";
+        logoImg.alt = "Food Logo";
+        logoCell.appendChild(logoImg);
+
+        const nameCell = document.createElement("td");
+        nameCell.textContent = item.name;
+
+        const priceCell = document.createElement("td");
+        priceCell.textContent = item.price;
+
+        row.appendChild(logoCell);
+        row.appendChild(nameCell);
+        row.appendChild(priceCell);
+
+        tbody.appendChild(row);
+    });
+
+    // Create the total row
+    const totalRow = document.createElement("tr");
+
+    const emptyCell = document.createElement("td");
+    const emptyCell2 = document.createElement("td");
+    const totalCell = document.createElement("td");
+
+    const totalAmt = cart.reduce(
+        (total, item) => total + Number(item.price.substring(1)),
+        0
+    );
+    totalCell.textContent = "$" + totalAmt;
+
     totalRow.appendChild(emptyCell);
     totalRow.appendChild(emptyCell2);
     totalRow.appendChild(totalCell);
-    
+
     tbody.appendChild(totalRow);
-    
+
     table.appendChild(tbody);
     cartBody.appendChild(table);
     menuCard.appendChild(cartBody);
